5a0bdde6c84d87f73bb68aadd00696338d4d0834
aiaflatunova <31651217+aiaflatunova@users.noreply.github.com>
2017-12-04 22:41:05 +0300

    Add files via upload

---
+++ hw5/hw5.py
@@ -0,0 +1,8 @@
+print('Input words: ')
+with open("new.txt", "w", encoding="utf - 8") as f:
+    word = input()
+    while word != " ":
+        if len(word) > 5:
+            f.write(word + "\n")
+with open("new.txt", encoding="utf - 8") as f:
+    print(f.read().strip())
