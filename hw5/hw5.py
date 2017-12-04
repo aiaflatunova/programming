8ce2aaa95ecbf3b26e3d65e29fdce4ffd2276a00
aiaflatunova <31651217+aiaflatunova@users.noreply.github.com>
2017-12-04 22:40:51 +0300

    Delete hw5.py

--- hw5/hw5.py
+++
@@ -1,9 +0,0 @@
-print('Input words: ')
-with open("new.txt", "w", encoding="utf - 8") as f:
-    word = input()
-    while word != " ":
-        if len(word) > 5:
-            f.write(word + "\n")
-        word = input()
-with open("new.txt", encoding="utf - 8") as f:
-    print(f.read().strip())
